4c9427c372dc81702743e8bf86961767ba92c8de
Nick <nick@nfreader.net>
2024-03-21 21:52:42 -0400

    Initial commit with the tg star logos and the NT logo

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Vector Assets
+
+Mostly SVGs of various TG assets, collected here in one place for your convenience.
+
+## Contributing 
+Run your SVGs through [`svgo`](https://svgo.dev/). The default configuration in this repository is preferred.
---
+++ svgo.config.mjs
@@ -0,0 +1,13 @@
+export default {
+    js2svg: { indent: 2, pretty: true },
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupIds: false,
+          },
+        },
+      },
+    ],
+  };
